98ca305005b633e43c3b0a8f9393b44d9cf82def
Chathura <crajapakse@itsupportme.by>
2019-08-16 17:06:15 +0300

    hello salesforce

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello Salesforce. test test test test!");
+        System.out.println("Hello Salesforce!");
+        System.out.println("This is a test project for Salesforce/Flosum");
     }
 }
